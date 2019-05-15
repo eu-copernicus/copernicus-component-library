4d61b0e7e115f8c4ae7e549a6ac9ae01bf5cfdd6
Codex42 <martin@codex42.be>
2019-05-15 12:38:39 +0200

    v0.2.1

--- assets/js/ccl-build.js
+++ assets/js/ccl-build.js
@@ -7,7 +7,7 @@ $(function() {
 	$(".ccl-site-nav li").addClass('ccl-side-navigation__item');
 	$(".ccl-site-nav a").each(function(){
 		$(this).addClass('ccl-side-navigation__link');
-		if (typeof $(this).next('<ul>') !== 'undefined'){
+		if (typeof $(this).next('ul') !== 'undefined'){
 			(this).addClass('ccl-expandable__button');
 		}
 	});
