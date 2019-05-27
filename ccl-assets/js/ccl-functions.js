94a8ee879304a5be8139b20d9505b3f81249d0a7
Codex42 <martin@codex42.be>
2019-05-27 04:01:16 +0200

    v0.7

--- ccl-assets/js/ccl-functions.js
+++ ccl-assets/js/ccl-functions.js
@@ -189,6 +189,41 @@ $(function() {
     })
 
 
+  $('.ccl-banner-top-container-slider').slick({
+    autoplay:true,
+    autoplaySpeed:3000
+  });
 
+$('.ccl-list-carousel').slick({
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }   
+  ]
+});
 
   });
